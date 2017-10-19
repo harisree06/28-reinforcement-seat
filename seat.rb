d7bf553d86d97d09047a70ca6b8cf1517e3414a2
Lan Phan <lanc_phan@hotmail.com>
2017-10-19 13:45:48 -0400

    looping through the outer and inner array to access all seats.

--- seat.rb
+++ seat.rb
@@ -1,6 +1,13 @@
-[
+seating_arrangement= [
   [nil, "Pumpkin", nil, nil],
   ["Socks", nil, "Mimi", nil],
   ["Gismo", "Shadow", nil, nil],
   ["Smokey","Toast","Pacha","Mau"]
 ]
+
+seating_arrangement.each do |row|
+  row.each_with_index do |value, index|
+    puts "Row #{index} seat #{value} is free."
+  end
+
+end
