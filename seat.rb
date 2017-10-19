f66f8ef7ec2db6f28cc782b503431dbdb99e8daf
Lan Phan <lanc_phan@hotmail.com>
2017-10-19 13:58:16 -0400

    all available seats from each row

--- seat.rb
+++ seat.rb
@@ -5,9 +5,12 @@ seating_arrangement= [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
-seating_arrangement.each do |row|
-  row.each_with_index do |value, index|
-    puts "Row #{index} seat #{value} is free."
+seating_arrangement.each_with_index do |row,row_number|
+  row.each_with_index do |person_name, seat_number|
+    if person_name == nil
+      puts "Row #{row_number+1} seat #{seat_number+1} is free."
+    end
+
   end
 
 end
